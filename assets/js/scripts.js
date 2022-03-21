de0adfab83426036965b2a3196a215c92387691e
Maria Meyer <mbean1216@icloud.com>
2022-03-21 17:39:03 -0500

    consoling cocktail name

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,27 @@
 const searchBtn = document.querySelector('button');
 
+// function to get random drink by ingredient user selects
+// function getDrink() {
+//     var userIngr = document.getElementById('ingredient').value
+//     fetch("https://cocktails3.p.rapidapi.com/search/byingredient/" + userIngr, {
+//         "method": "GET",
+//         "headers": {
+//             "x-rapidapi-host": "cocktails3.p.rapidapi.com",
+//             "x-rapidapi-key": "7cdce6aa00mshb6271779e3ce1f6p1cdc87jsn64cadd3d366d"
+//         }
+//     })
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(data) {
+//         console.log(data);
+//     })
+//     .catch(err => {
+//         console.error(err);
+//     });
+// }
 function getDrink() {
-    var userIngr = document.getElementById('ingredient').value
-    console.log(userIngr);
-    fetch("https://cocktails3.p.rapidapi.com/search/byingredient/" + userIngr, {
+    fetch("https://cocktails3.p.rapidapi.com/random", {
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "cocktails3.p.rapidapi.com",
@@ -13,8 +31,11 @@ function getDrink() {
     .then(function(response) {
         return response.json();
     })
-    .then(function(data) {
+    .then(function(data){
         console.log(data);
+        for (var i = 0; i < data.body[0].length; i++);
+        var cocktailName = `${data.body[i].name}`
+        console.log(cocktailName);
     })
     .catch(err => {
         console.error(err);
