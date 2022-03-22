69e0e5c040618b9a6588891d884bb675a7d5acf4
Maria Meyer <mbean1216@icloud.com>
2022-03-22 15:30:01 -0500

    creating random cocktail button

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,77 +1,45 @@
-const searchBtn = document.getElementById('search');
-const drinkSection = document.getElementById('cocktail');
+var searchBtn = document.getElementById('search');
+var randoDrink = document.getElementById('randoDrink');
 var cocktailTitle = document.getElementById('cocktailTitle');
-var ingredientUl = document.getElementById('ingredients');
-var currentCocktailSearch = [];
 
 // creates cards for cocktails
 function createCard(cocktailName, ingredients) {
-    // var elementLi = document.createElement('li');
-    // elementLi.textContent = cocktailName;
-    // cocktailIngr.appendChild(elementLi);
-    var divRow = document.createElement('div');
-    divRow.classList.add('row');
-    var divCol = document.createElement('div');
-    divCol.classList.add('col', 's12', 'm6');
+    // creates card for cocktail name
+    var mainDiv = document.createElement('div');
+    mainDiv.classList.add('row')
     var divCard = document.createElement('div');
-    divCard.classList.add('card', 'blue-grey', 'darken-1');
+    divCard.classList.add('col', 's2','card', 'indigo', 'darken-4');
     var divCont = document.createElement('div');
     divCont.classList.add('card-content', 'white-text');
     var divTitle = document.createElement('span');
-    divTitle.classList.add('card-title');
     divTitle.textContent = cocktailName;
     divTitle.setAttribute('value', cocktailName);
 
-    divTitle.addEventListener('click', nameIngredients);
 
-    cocktailTitle.appendChild(divRow);
-    divRow.appendChild(divCol);
-    divCol.appendChild(divCard);
+    mainDiv.appendChild(divCard);
     divCard.appendChild(divCont);
     divCont.appendChild(divTitle);
 
-
-    var divRow2 = document.createElement('div');
-    divRow2.classList.add('row');
-    var divCol2 = document.createElement('div');
-    divCol2.classList.add('col', 's12', 'm6');
+    // creates card for ingredients
     var divCard2 = document.createElement('div');
-    divCard2.classList.add('card', 'blue-grey', 'darken-1');
+    divCard2.classList.add('col', 's10', 'card', 'indigo', 'accent-1');
     var divCont2 = document.createElement('div');
     divCont2.classList.add('card-content', 'white-text');
     var divTitle2 = document.createElement('span');
-    divTitle2.classList.add('card-title');
     divTitle2.textContent = ingredients;
-    console.log(divTitle2);
+    
     
 
-    cocktailTitle.appendChild(divRow2);
-    divRow2.appendChild(divCol2);
-    divCol2.appendChild(divCard2);
+    mainDiv.appendChild(divCard2);
     divCard2.appendChild(divCont2);
     divCont2.appendChild(divTitle2);
 
-
-}
-
-function nameIngredients(e) {
-    e.preventDefault();
-    console.log(e.target.innerText);
-    for (let index = 0; index < currentCocktailSearch.length; index++) {
-        const element = currentCocktailSearch[index];
-        if (element.name === e.target.innerText) {
-            console.log(element);
-            // create card with ingredients
-        }
-    }
-    // for (let index = 0; index < array.length; index++) {
-    //     const element = array[index];
-        
-    // }
+    mainDiv.style.display = 'flex';
+    cocktailTitle.appendChild(mainDiv);
 }
 
 
-// function to get random drink by ingredient user selects
+// function to get drinks by ingredient user selects
 function getDrink() {
     var userIngr = document.getElementById('ingredientType').value
     fetch("https://cocktails3.p.rapidapi.com/search/byingredient/" + userIngr, {
@@ -87,57 +55,54 @@ function getDrink() {
     .then(function(data) {
         currentCocktailSearch = [];
         currentCocktailSearch = data.body[0];
-        console.log(currentCocktailSearch);
         $(cocktailTitle).empty();
         var cocktails = data.body[0];
         var numCocktails = cocktails.length <= 5 ? cocktails.length : 5;
         for (var i = 0; i < numCocktails; i++) {
         createCard(cocktails[i].name, cocktails[i].ingredients);
         }
-        // var cocktailName = `${data.body[i].name}`
-        // console.log(cocktailName);
+    })
+    .catch(err => {
+        console.error(err);
+    });
+}
+
+// function to get random alcoholic drink
+function getRandoDrink() {
+    fetch("https://cocktails3.p.rapidapi.com/random", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "cocktails3.p.rapidapi.com",
+            "x-rapidapi-key": "7cdce6aa00mshb6271779e3ce1f6p1cdc87jsn64cadd3d366d"
+        }
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data){
+        cocktailTitle.innerHTML = ''
+        // get information about random cocktail
+        for (var i = 0; i < data.body[0].length; i++);
+        var cocktailName = `${data.body[i].name}`
+        var cocktailIngredients = `${data.body[i].ingredients}`
+        // creating html elements for pulled data
+        var ranDiv = document.createElement('div');
+        var h3 = document.createElement('h3');
+        var ingrLi = document.createElement('p')
+        h3.innerHTML = cocktailName;
+        ingrLi.innerHTML = cocktailIngredients;
+        
+        ranDiv.appendChild(h3);
+        h3.appendChild(ingrLi);
+        cocktailTitle.appendChild(ranDiv);
         
     })
     .catch(err => {
         console.error(err);
     });
 }
-searchBtn.addEventListener('click',getDrink);
+
+searchBtn.addEventListener('click', getDrink);
+randoDrink.addEventListener('click', getRandoDrink)
 
 
-
-// // function to get alcoholic drink
-// function getAlcDrink() {
-//     fetch("https://cocktails3.p.rapidapi.com/random", {
-//         "method": "GET",
-//         "headers": {
-//             "x-rapidapi-host": "cocktails3.p.rapidapi.com",
-//             "x-rapidapi-key": "7cdce6aa00mshb6271779e3ce1f6p1cdc87jsn64cadd3d366d"
-//         }
-//     })
-//     .then(function(response) {
-//         return response.json();
-//     })
-//     .then(function(data){
-//         drinkSection.innerHTML = '';
-//         drinkIngr.innerHTML = '';
-//         console.log(data);
-//         for (var i = 0; i < data.body[0].length; i++);
-//         var cocktailName = `${data.body[i].name}`
-//         var cocktailIngredients = `${data.body[i].ingredients}`
-//         console.log(cocktailName);
-//         console.log(cocktailIngredients);
-//         var h3 = document.createElement('h3');
-//         var ingrLi = document.createElement('li')
-//         h3.innerHTML = cocktailName;
-//         ingrLi.innerHTML = cocktailIngredients;
-//         drinkIngr.appendChild(ingrLi);
-//         drinkSection.appendChild(h3);
-//     })
-//     .catch(err => {
-//         console.error(err);
-//     });
-// }
-
-
-// ranAlco.addEventListener('click', getAlcDrink);
